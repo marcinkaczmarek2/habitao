afe74eaf045877ef24896c5e33c67006f56b096e
szymonmelait <250346@edu.p.lodz.pl>
2025-10-22 09:35:54 +0200

    small correction

--- app/src/main/java/app/habitao/ui/components/SettingsCategoryCard.kt
+++ app/src/main/java/app/habitao/ui/components/SettingsCategoryCard.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
