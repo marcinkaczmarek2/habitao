a24fff2e3af69a126b7ac421a3713181ede7ca9d
marcinkaczmarek2 <titnet112pl@gmail.com>
2025-10-26 19:48:38 +0100

    Add SettingsAccountScreen (not logged in version), SettingsPrivacyScreen

--- app/src/main/java/app/habitao/ui/screens/SettingsAccountScreen.kt
+++ app/src/main/java/app/habitao/ui/screens/SettingsAccountScreen.kt
@@ -2,30 +2,45 @@ package app.habitao.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import app.habitao.ui.components.LowerNavigationMenu
+import app.habitao.ui.components.SettingsLoginButton
+import app.habitao.ui.components.SettingsTopBar
 import app.habitao.ui.theme.IconTextNonActive
+import app.habitao.ui.theme.MainBackgroundColor
 
 @Composable
 fun SettingsAccountScreenInitialize(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF121212))
+            .background(MainBackgroundColor)
     ) {
-        Text(
-            text = "account screen",
-            fontSize = 20.sp,
-            color = IconTextNonActive,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+        ) {
+            SettingsTopBar(
+                title = "Account",
+                navController = navController
+            )
+            Spacer(modifier = Modifier.height(350.dp))
+            SettingsLoginButton()
+        }
 
         Box(
             modifier = Modifier
