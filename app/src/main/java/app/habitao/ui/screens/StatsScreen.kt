bfa4263e7e69d3d81cd6c1b210f17eed97726130
Szymon Zajączkowski <250375@edu.p.lodz.pl>
2025-12-10 03:10:04 +0100

    Stats screen - landscape improvement

--- app/src/main/java/app/habitao/ui/screens/StatsScreen.kt
+++ app/src/main/java/app/habitao/ui/screens/StatsScreen.kt
@@ -1,15 +1,20 @@
 package app.habitao.ui.screens
 
+import android.content.res.Configuration
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -17,6 +22,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -81,6 +87,9 @@ fun StatsScreenInitialize(navController: NavController, viewModel: StatsViewMode
 
     Log.d("Stats", "karmaToNextLv: $karmaToNextLv")
 
+    //conf for orientation
+    val conf = LocalConfiguration.current
+
     //GUI
     val scrollState = rememberScrollState()
 
@@ -99,25 +108,51 @@ fun StatsScreenInitialize(navController: NavController, viewModel: StatsViewMode
                 .verticalScroll(scrollState)
                 .padding(start = 16.dp, top = 0.dp, end = 16.dp, bottom = 112.dp)
         ) {
-            //total karma
-            StatsTotalKarma(totalKarma)
+            if (conf.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                //total karma
+                StatsTotalKarma(totalKarma)
 
-            //karma progress
-            StatsKarmaProgress(karmaProgress, karmaToNextLv, currentLevel)
+                //karma progress
+                StatsKarmaProgress(karmaProgress, karmaToNextLv, currentLevel)
 
-            //bonsai tree - decoration
-            //<a href="https://www.flaticon.com/free-icons/bonsai" title="bonsai icons">Bonsai icons created by Ylivdesign - Flaticon</a>
-            //TODO: those images could be improved
-            //VALUES [-1; 5] -> -1 dead bonsai
-            //TODO: negative karma/sth for dead tree
+                //bonsai tree - decoration
+                //<a href="https://www.flaticon.com/free-icons/bonsai" title="bonsai icons">Bonsai icons created by Ylivdesign - Flaticon</a>
+                //TODO: those images could be improved
+                //VALUES [-1; 5] -> -1 dead bonsai
+                //TODO: negative karma/sth for dead tree
 
-            StatsBonsai(currentLevel)
+                StatsBonsai(currentLevel)
 
-            //elements description
-            StatsElemDescr(airPts, firePts, waterPts, earthPts)
+                //elements description
+                StatsElemDescr(airPts, firePts, waterPts, earthPts)
 
-            //graph box
-            StatsElemGraphBox(airPts, firePts, waterPts, earthPts)
+                //graph box
+                StatsElemGraphBox(airPts, firePts, waterPts, earthPts)
+            }
+            else {
+                Row {
+                    Column(modifier = Modifier.width(500.dp)) {
+                        //total karma
+                        StatsTotalKarma(totalKarma)
+
+                        //karma progress
+                        StatsKarmaProgress(karmaProgress, karmaToNextLv, currentLevel)
+                    }
+
+                    Column(modifier = Modifier.fillMaxWidth().padding(start = 10.dp)) {
+                        //bonsai tree - decoration
+                        StatsBonsai(currentLevel)
+
+                        //elements description
+                        StatsElemDescr(airPts, firePts, waterPts, earthPts)
+                    }
+                }
+
+                Box(modifier = Modifier.width(400.dp)) {
+                    //graph box
+                    StatsElemGraphBox(airPts, firePts, waterPts, earthPts)
+                }
+            }
 
             //NOTE: use it for all information visibility
             StatsDebugBox(viewModel)
